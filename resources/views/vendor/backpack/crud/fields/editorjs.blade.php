0c50dea6ceda4855880f17da517cb6c1d1036f90
Shoaib Sharif <shoaib19j@gmail.com>
2021-03-20 15:54:02 -0700

    editorjs: added tools

--- resources/views/vendor/backpack/crud/fields/editorjs.blade.php
+++ resources/views/vendor/backpack/crud/fields/editorjs.blade.php
@@ -8,7 +8,7 @@
     <label>{!! $field['label'] !!}</label>
     @include('crud::fields.inc.translatable_icon')
 
-    <div id="editorjs" data-init-function="techdiary__editorjsFunction"></div>
+    <div id="editorjs" data-init-function="techdiary__editorjsFunction" style="border: 1px solid #dddddd"></div>
 
     <input type="hidden" name="{{ $field['name'] }}" id="editorjs-{{$field['name']}}">
 
@@ -27,11 +27,68 @@
 
     {{-- push things in the after_scripts section --}}
     @push('crud_fields_scripts')
+
+        <script src="https://cdn.jsdelivr.net/npm/@editorjs/header@latest"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@editorjs/list@latest"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@editorjs/checklist@latest"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@editorjs/quote@latest"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@editorjs/link@latest"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@editorjs/marker@latest"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@editorjs/embed@latest"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@editorjs/table@latest"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@editorjs/warning@latest"></script>
+        <script src="https://cdn.jsdelivr.net/gh/paraswaykole/editor-js-code@latest/dist/bundle.js"></script>
+
         <script src="https://cdn.jsdelivr.net/npm/@editorjs/editorjs@latest"></script>
 
         <script>
             const editor = new EditorJS({
                 holder: 'editorjs',
+                placeholder: "Start your diary",
+                tools: {
+                    /**
+                     * Each Tool is a Plugin. Pass them via 'class' option with necessary settings {@link docs/tools.md}
+                     */
+                    header: {
+                        class: Header,
+                        inlineToolbar: ['marker'],
+                        config: {
+                            placeholder: 'Header',
+                            levels: [2, 3],
+                        },
+                        shortcut: 'CMD+SHIFT+H',
+                    },
+                    list: {
+                        class: List,
+                        inlineToolbar: true,
+                        shortcut: 'CMD+SHIFT+L',
+                    },
+                    checklist: {
+                        class: Checklist,
+                        inlineToolbar: true,
+                    },
+                    quote: {
+                        class: Quote,
+                        inlineToolbar: true,
+                        config: {
+                            quotePlaceholder: 'Enter a quote',
+                            captionPlaceholder: "Quote's author",
+                        },
+                        shortcut: 'CMD+SHIFT+O',
+                    },
+                    warning: Warning,
+                    marker: {
+                        class: Marker,
+                        shortcut: 'CMD+SHIFT+M',
+                    },
+                    code: CodeTool,
+                    embed: Embed,
+                    table: {
+                        class: Table,
+                        inlineToolbar: true,
+                        shortcut: 'CMD+ALT+T',
+                    },
+                },
                 data: {
                     blocks: {!! $blocks !!}
                 },
@@ -41,6 +98,7 @@
                     })
                 },
             });
+
         </script>
     @endpush
 @endif
