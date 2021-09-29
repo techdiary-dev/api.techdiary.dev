921ca560e8e531b5a1f2ceb2d05fd02d86ac671a
Shoaib Sharif <shoaib19j@gmail.com>
2021-09-28 22:28:32 -0700

    added svg on parmalink

--- app/TechDiary/TDMarkdown.php
+++ app/TechDiary/TDMarkdown.php
@@ -9,7 +9,6 @@ use League\CommonMark\Extension\Table\TableExtension;
 use Torchlight\Commonmark\V2\TorchlightExtension;
 
 
-
 class TDMarkdown
 {
 
@@ -22,7 +21,6 @@ class TDMarkdown
     {
         $this->markdown = $markdown;
     }
-
 
 
     /**
@@ -42,14 +40,16 @@ class TDMarkdown
                 'min_heading_level' => 1,
                 'max_heading_level' => 6,
                 'title' => 'Permalink',
-                'symbol' => HeadingPermalinkRenderer::DEFAULT_SYMBOL
+                'symbol' => '<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+  <path fill-rule="evenodd" d="M12.586 4.586a2 2 0 112.828 2.828l-3 3a2 2 0 01-2.828 0 1 1 0 00-1.414 1.414 4 4 0 005.656 0l3-3a4 4 0 00-5.656-5.656l-1.5 1.5a1 1 0 101.414 1.414l1.5-1.5zm-5 5a2 2 0 012.828 0 1 1 0 101.414-1.414 4 4 0 00-5.656 0l-3 3a4 4 0 105.656 5.656l1.5-1.5a1 1 0 10-1.414-1.414l-1.5 1.5a2 2 0 11-2.828-2.828l3-3z" clip-rule="evenodd" />
+</svg>'
             ],
         ];
         $converter = new CommonMarkConverter($config);
         $converter->getEnvironment()->addExtension(new HeadingPermalinkExtension());
         $converter->getEnvironment()->addExtension(new TableExtension());
 
-        return (string) $converter->convertToHtml($this->markdown ?: "");
+        return (string)$converter->convertToHtml($this->markdown ?: "");
     }
 
 }
