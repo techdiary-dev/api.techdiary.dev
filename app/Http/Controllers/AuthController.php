dcf2e0fdfedff4ad5833c1b181259ee0f047167d
kingrayhan <rayhan095@gmail.com>
2022-01-12 03:48:41 +0600

    fix local

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -17,22 +17,21 @@ use Laravel\Socialite\Two\InvalidStateException;
 
 class AuthController extends Controller
 {
-
     /**
      * Register account
      * @param RegisterRequest $request
      * @return \Illuminate\Http\JsonResponse
      */
-//    public function register(RegisterRequest $request)
-//    {
-//        $user = User::create($request->all(['name', 'username', 'email', 'password']));
-//
-//        return response()->json([
-//            'message' => 'Registered successfully',
-//            'user' => $user
-//        ]);
-//    }
-//
+    //    public function register(RegisterRequest $request)
+    //    {
+    //        $user = User::create($request->all(['name', 'username', 'email', 'password']));
+    //
+    //        return response()->json([
+    //            'message' => 'Registered successfully',
+    //            'user' => $user
+    //        ]);
+    //    }
+    //
 
     /**
      * Login using email and password
@@ -40,42 +39,42 @@ class AuthController extends Controller
      * @return array
      * @throws AuthenticationException
      */
-//    public function getTokenUsingCredential(LoginRequest $request)
-//    {
-//        if (!auth()->guard()->attempt($request->all()))
-//            throw new AuthenticationException('Invalid credential');
-//
-//        return [
-//            'token' => Authentication::createToken(auth()->user())
-//        ];
-//    }
+    //    public function getTokenUsingCredential(LoginRequest $request)
+    //    {
+    //        if (!auth()->guard()->attempt($request->all()))
+    //            throw new AuthenticationException('Invalid credential');
+    //
+    //        return [
+    //            'token' => Authentication::createToken(auth()->user())
+    //        ];
+    //    }
 
     /**
      * Authenticated user's tokens
      * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
      */
-//    public function myTokens()
-//    {
-//        return MyTokensResource::collection(auth()->user()->tokens);
-//    }
+    //    public function myTokens()
+    //    {
+    //        return MyTokensResource::collection(auth()->user()->tokens);
+    //    }
 
-//    public function revokeToken($id)
-//    {
-//        auth()->user()->tokens()->find($id)->delete();
-//
-//        return response()->json([
-//            'message' => 'Token revoked successfully'
-//        ]);
-//    }
+    //    public function revokeToken($id)
+    //    {
+    //        auth()->user()->tokens()->find($id)->delete();
+    //
+    //        return response()->json([
+    //            'message' => 'Token revoked successfully'
+    //        ]);
+    //    }
 
-//    public function logout()
-//    {
-//        auth()->user()->currentAccessToken()->delete();
-//
-//        return response()->json([
-//            'message' => 'Successfully logged out'
-//        ]);
-//    }
+    //    public function logout()
+    //    {
+    //        auth()->user()->currentAccessToken()->delete();
+    //
+    //        return response()->json([
+    //            'message' => 'Successfully logged out'
+    //        ]);
+    //    }
 
     /**
      *
@@ -84,33 +83,48 @@ class AuthController extends Controller
      */
     public function oauthRedirect($service)
     {
-        return Socialite::driver($service)->stateless()->redirect();
+        return Socialite::driver($service)
+            ->stateless()
+            ->redirect();
     }
 
     public function oauthCallback($service)
     {
         try {
-            $socialServiceUser = Socialite::driver($service)->stateless()->user();
+            $socialServiceUser = Socialite::driver($service)
+                ->stateless()
+                ->user();
 
             $social_user = UserSocial::where([
-                ['service', $service],
-                ['service_uid', $socialServiceUser->id]
+                ["service", $service],
+                ["service_uid", $socialServiceUser->id],
             ])->first();
 
-            if ($social_user) $user = $social_user->user;
-            else if (!$user = User::whereEmail($socialServiceUser->email)->first()) {
-
+            if ($social_user) {
+                $user = $social_user->user;
+            } elseif (
+                !($user = User::whereEmail($socialServiceUser->email)->first())
+            ) {
                 $user = new User([
-                    'username' => $socialServiceUser->nickname ?? strtolower(explode("@", $socialServiceUser->email)[0] . Str::random(4)),
-                    'name' => $socialServiceUser->name ?? Str::random(6),
-                    'email' => $socialServiceUser->email,
-                    'profilePhoto' => $socialServiceUser?->avatar,
-                    'bio' => collect($socialServiceUser->user)->has('bio') ? $socialServiceUser->user['bio'] : null,
+                    "username" =>
+                        $socialServiceUser->nickname ??
+                        strtolower(
+                            explode("@", $socialServiceUser->email)[0] .
+                                Str::random(4)
+                        ),
+                    "name" => $socialServiceUser->name ?? Str::random(6),
+                    "email" => $socialServiceUser->email,
+                    "profilePhoto" => $socialServiceUser?->avatar,
+                    "bio" => collect($socialServiceUser->user)->has("bio")
+                        ? $socialServiceUser->user["bio"]
+                        : null,
                 ]);
 
-                if ($service == 'github') {
+                if ($service == "github") {
                     $user->social_links = [
-                        'github' => 'https://github.com/' . $socialServiceUser->nickname
+                        "github" =>
+                            "https://github.com/" .
+                            $socialServiceUser->nickname,
                     ];
                 }
                 $user->save();
@@ -119,45 +133,49 @@ class AuthController extends Controller
             if (!$social_user) {
                 NewUserCreated::dispatch($user);
                 $user->socialProviders()->create([
-                    'service' => $service,
-                    'service_uid' => $socialServiceUser->id
+                    "service" => $service,
+                    "service_uid" => $socialServiceUser->id,
                 ]);
             }
 
+            $signedRoute = URL::temporarySignedRoute(
+                "oauth-signed-login",
+                now()->addMinutes(10),
+                [
+                    "user_id" => $user->id,
+                ]
+            );
+            $signedToken = explode("?", $signedRoute)[1];
 
-            $signedRoute = URL::temporarySignedRoute('oauth-signed-login', now()->addMinutes(10), [
-                'user_id' => $user->id,
-            ]);
-            $signedToken = explode('?', $signedRoute)[1];
-
-
-            $redirect_url = env('CLIENT_URL') . '/auth/oauth-callback?' . $signedToken;
+            $redirect_url =
+                env("CLIENT_BASE_URL") . "/auth/oauth-callback?" . $signedToken;
             return redirect($redirect_url);
-
+            return env("CLIENT_URL");
         } catch (InvalidStateException $e) {
-            return $this->redirect(env('CLIENT_URL') . '?error=1');
+            return $this->redirect(env("CLIENT_URL") . "?error=1");
         }
     }
 
-
     public function oauthSignedLogin(\Illuminate\Http\Request $request)
     {
-        $userId = $request->get('user_id');
+        $userId = $request->get("user_id");
         auth()->loginUsingId($userId);
 
         return response()->json([
-            'message' => 'Successfully logged in through cookie',
-            'user' => auth()->user()
+            "message" => "Successfully logged in through cookie",
+            "user" => auth()->user(),
         ]);
     }
 
     public function updateProfile(UpdateProfileRequest $request)
     {
-        auth()->user()->update($request->all());
+        auth()
+            ->user()
+            ->update($request->all());
 
         return response()->json([
-            'message' => 'Profile Updated successfully',
-            'data' => auth()->user()
+            "message" => "Profile Updated successfully",
+            "data" => auth()->user(),
         ]);
     }
 
@@ -166,14 +184,16 @@ class AuthController extends Controller
      * @param UpdateBasicProfileSettingsRequest $request
      * @return \Illuminate\Http\JsonResponse
      */
-    public function updateBasicProfileSettings(UpdateBasicProfileSettingsRequest $request)
-    {
-        auth()->user()->update($request->all());
+    public function updateBasicProfileSettings(
+        UpdateBasicProfileSettingsRequest $request
+    ) {
+        auth()
+            ->user()
+            ->update($request->all());
 
         return response()->json([
-            'message' => 'Profile Updated successfully',
-            'data' => auth()->user()
+            "message" => "Profile Updated successfully",
+            "data" => auth()->user(),
         ]);
     }
-
 }
