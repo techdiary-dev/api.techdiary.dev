df049631f383c0199f52c44078bce22eafad3746
kingrayhan <rayhan095@gmail.com>
2022-01-17 23:31:38 +0600

    fix rss fee

--- app/Http/Controllers/RSSFeedController.php
+++ app/Http/Controllers/RSSFeedController.php
@@ -11,7 +11,7 @@ class RSSFeedController extends Controller
 {
     public function articles()
     {
-        $articles = Article::where([ 'isPublished' => true ])->limit(50)->get();
+        $articles = Article::where([ 'isPublished' => true ])->limit(50)->latest()->get();
         return response()->view('rss.articles', [
             'articles' => $articles
         ])->header('Content-Type', 'application/xml');
