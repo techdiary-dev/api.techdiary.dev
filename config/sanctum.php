9d3e40e9d9fdcf4f045f7ca3ca778b0bbe7cdd9f
KingRayhan <rayhan095@gmail.com>
2023-04-11 16:54:17 +0600

    fix sanctum domain

--- config/sanctum.php
+++ config/sanctum.php
@@ -15,15 +15,16 @@ return [
     |
     */
 
-//    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
-//        '%s%s',
+    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
+        '%s%s',
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
+        Sanctum::currentApplicationUrlWithPort()
+    ))),
+//    'stateful' => explode(',', env(
+//        'SANCTUM_STATEFUL_DOMAINS',
 //        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
-//        Sanctum::currentApplicationUrlWithPort()
-//    ))),
-    'stateful' => explode(',', env(
-        'SANCTUM_STATEFUL_DOMAINS',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1'
-    )),
+//    )),
+
 
     /*
     |--------------------------------------------------------------------------
@@ -37,7 +38,7 @@ return [
     |
     */
 
-    'guard' => ['web'],
+    'guard' => ['api'],
 
     /*
     |--------------------------------------------------------------------------
