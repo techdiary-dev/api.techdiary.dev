8a8214571bbfc46fdb424d8bb3c834648da3cb60
kingrayhan <rayhan095@gmail.com>
2024-05-19 04:56:13 +0600

    oauth login endpoint

--- routes/api-routes/auth.php
+++ routes/api-routes/auth.php
@@ -1,10 +1,15 @@
 <?php
 
-use App\Http\Controllers\Auth\AccessTokenController;
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\Auth\OAuthController;
 use App\Http\Controllers\Auth\PersonalAccessTokenController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('test', function () {
+    return response()->json([
+        'message' => 'test',
+    ]);
+})->name('password.reset');
 
 Route::group(['prefix' => 'auth'], function () {
     Route::post('signed-login', [AuthController::class, 'signedLogin'])
